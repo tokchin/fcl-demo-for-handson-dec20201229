e73de6b0e283b522874570ea87d570bea3246458
Yugo <yugo.tokuchi@gmial.com>
2020-12-17 02:22:10 +0900

    Remove getLatestBlock ScriptTwo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-import Section from './components/Section'
-import Header from './components/Header'
+import Section from "./components/Section";
+import Header from "./components/Header";
 
-import GetLatestBlock from './demo/GetLatestBlock'
-import GetAccount from './demo/GetAccount'
-import ScriptOne from "./demo/ScriptOne"
-import ScriptTwo from './demo/ScriptTwo'
-import Authenticate from './demo/Authenticate'
-import UserInfo from './demo/UserInfo'
-import SendTransaction from './demo/SendTransaction'
-import DeployContract from './demo/DeployContract'
-import InteractWithContract from './demo/InteractWithContract'
+import GetAccount from "./demo/GetAccount";
+import ScriptOne from "./demo/ScriptOne";
+import Authenticate from "./demo/Authenticate";
+import UserInfo from "./demo/UserInfo";
+import SendTransaction from "./demo/SendTransaction";
+import DeployContract from "./demo/DeployContract";
+import InteractWithContract from "./demo/InteractWithContract";
 
 const Wrapper = styled.div`
   font-size: 13px;
@@ -23,17 +21,11 @@ function App() {
   return (
     <Wrapper>
       <Section>
-        <Header>READ from FCL</Header>
-        <GetLatestBlock />
-        <GetAccount />
-        <ScriptOne />
-        <ScriptTwo />
-      </Section>
-
-      <Section>
         <Header>FCL wallet interactions</Header>
         <Authenticate />
         <UserInfo />
+        <GetAccount />
+        <ScriptOne />
         <SendTransaction />
         <DeployContract />
         <InteractWithContract />
